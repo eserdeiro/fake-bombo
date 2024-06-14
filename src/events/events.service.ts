364d394d7e589160df63decc1cb7bb435f5f3c9f
eserdeiro <eserdeiro@gmail.com>
2024-06-14 01:03:26 -0300

    ( Fix ) Problem | ( Add ) handle exceptions

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,6 +8,8 @@ import { Ticket } from './entities/ticket.entity';
 
 @Injectable()
 export class EventsService {
+
+  private readonly logger = new Logger('EventsService')
 
   constructor(
     @InjectRepository(Event)
@@ -19,21 +21,26 @@ export class EventsService {
   async create(createEventDto: CreateEventDto) {
     try {
       const event = this.eventRepository.create(createEventDto);
+      event.tickets = this.ticketRepository.create(createEventDto.tickets);
+      event.tickets.forEach(ticket => {
+        ticket.event = event;
+      });
       await this.eventRepository.save(event);
 
-      const tickets = createEventDto.tickets.map(ticketDto => {
-        const ticket = this.ticketRepository.create(ticketDto);
-        ticket.event = event;
-        return ticket;
-      });
+      const result = {
+        ...event,
+        tickets: event.tickets.map(ticket => {
+          const { event, ...ticketData } = ticket;
+          return ticketData;
+        })
+      };
 
-      await this.ticketRepository.save(tickets);
+      return result;
 
-      return event;
     } catch (error) {
-      console.log(error.detail)
-      throw new InternalServerErrorException('error')
+      this.handleExeptions(error)
     }
+
   }
 
   findAll() {
@@ -50,5 +57,13 @@ export class EventsService {
 
   remove(id: number) {
     return `This action removes a #${id} event`;
+  }
+
+  private handleExeptions(error: any) {
+    if (error.code === '23505')
+      throw new BadRequestException(error.detail)
+
+    this.logger.error(error)
+    throw new InternalServerErrorException('Unexpected error, check the server logs')
   }
 }
