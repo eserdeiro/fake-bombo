df1874c97e83193534aef15d4d6ca6fceebd1dae
eserdeiro <eserdeiro@gmail.com>
2024-06-14 22:41:54 -0300

    ( Edit ) files controller

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,8 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, ParseFilePipe, FileTypeValidator, MaxFileSizeValidator, HttpException, HttpStatus, ValidationError, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
+
+  @Post('event')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)', }),
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 3 })
+        ],
+        exceptionFactory: (error: string) => handleFileError(error)
+      })
+
+    ) file: Express.Multer.File,
+  ) {
+    return file
+  }
+}
+
+function handleFileError(error: string): BadRequestException {
+  let message: string;
+
+  switch (true) {
+    case error.includes('expected type is'):
+      message = 'Invalid file type. Only PNG | JPEG | JPG are allowed.';
+      break;
+    case error.includes('expected size is less than'):
+      message = 'File size too large. Maximum size allowed is 3MB.';
+      break;
+    default:
+      message = 'An error occurred during file validation.';
+      break;
+  }
+
+  return new BadRequestException(message);
 
 }
