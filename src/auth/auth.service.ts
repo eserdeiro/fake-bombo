598a851efc9e446a456aecbdf85c3bdfb0d94c9c
eserdeiro <eserdeiro@gmail.com>
2024-06-16 18:39:00 -0300

    Encrypt password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcr from "bcrypt";
 import { ErrorHandlingService } from 'src/common/error-handling/error-handling.service';
 
 @Injectable()
@@ -13,15 +14,19 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly errorHandlingService: ErrorHandlingService
-  ) {
-
-  }
+  ) { }
 
   async create(createUserDto: CreateUserDto) {
-
     try {
-      const user = await this.userRepository.create(createUserDto)
+      const { password, ...userData } = createUserDto
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcr.hashSync(password, 10)
+      })
+
       await this.userRepository.save(user)
+      delete user.password
       return user
     } catch (error) {
       this.errorHandlingService.handleDatabaseErrors(error)
